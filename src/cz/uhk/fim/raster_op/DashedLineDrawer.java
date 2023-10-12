ce7d69d80b21bc4207dfc1de90b934e17ee85e53
MartmatiX <martin.malir@gmail.com>
2023-10-12 21:46:32 +0200

    Comments

--- src/cz/uhk/fim/raster_op/DashedLineDrawer.java
+++ src/cz/uhk/fim/raster_op/DashedLineDrawer.java
@@ -6,7 +6,7 @@ import cz.uhk.fim.raster_data.Raster;
 /**
  * This class represents a dashed line that uses Naive Line algorithm to draw a line with added spacing between pixels to create feeling of dashed line being drawn
  * The lines are being drawn until certain set limit is reached, in default 5, 5. Meaning the dash will be drawn for 5 pixels then there will be 5 pixel delay.
- * i.e. - represents dash, * represents space -> with limit 5, 5 -> -----*****-----*****-----*****
+ * i.e. - represents dash, * represents space -> with limit 5, 5 -> -----*****-----*****-----*****-----
  */
 public class DashedLineDrawer implements Liner {
 
